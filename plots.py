4f4240d9a570fbd02b3aec96b150c68100828aea
YAO_LEI XU <yaoleixu1996@gmail.com>
2020-09-08 16:28:10 +0100

    Plotting european currencies performance

--- plots.py
+++ plots.py
@@ -3,16 +3,18 @@ import pandas as pd
 import glob
 import matplotlib.pyplot as plt
 
-ep = '15'
+
+plt.style.use('fivethirtyeight')
 files = glob.glob( 'results/**.csv' )
-col_names = [(file.split('GTN ')[-1]).split('testing')[0] for file in files if 'testing' in file]
+
+
+# Returns averaged across models per episodes plotting
+ep = '14'
+col_names = [(file.split('GTN ')[-1]).split(' testing')[0] for file in files if 'testing' in file]
 
 testing_df = [pd.read_csv(file, index_col=0, header=0)[ep] for file in files if 'testing' in file]
 testing_df = pd.concat(testing_df, axis=1)
 testing_df.columns = col_names
 
-training_df = [pd.read_csv(file, index_col=0, header=0)[ep] for file in files if 'training' in file]
-training_df = pd.concat(training_df, axis=1)
-training_df.columns = col_names
-
-testing_df.mean(1).cumsum().plot()
+sel = ['EURUSD', 'USDCHF', 'GBPUSD', 'USDNOK', 'USDSEK'] # european currencies only
+testing_df[sel].mean(1).cumsum().plot()
