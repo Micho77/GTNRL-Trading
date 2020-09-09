6a44aa6724472db7fa75a17f970beba2ee9e5a3d
YAO_LEI XU <yaoleixu1996@gmail.com>
2020-09-09 10:35:59 +0100

    Added performance metrics

--- plots.py
+++ plots.py
@@ -5,16 +5,60 @@ import matplotlib.pyplot as plt
 
 
 plt.style.use('fivethirtyeight')
-files = glob.glob( 'results/**.csv' )
 
 
-# Returns averaged across models per episodes plotting
-ep = '14'
-col_names = [(file.split('GTN ')[-1]).split(' testing')[0] for file in files if 'testing' in file]
+def get_testing_df (agent='GTN', ep='14'):
 
-testing_df = [pd.read_csv(file, index_col=0, header=0)[ep] for file in files if 'testing' in file]
-testing_df = pd.concat(testing_df, axis=1)
-testing_df.columns = col_names
+    # Returns averaged across models per episodes plotting
+    files = glob.glob('results/**.csv')
+    files = [file for file in files if ('testing' in file) and (agent in file)]
+    col_names = [(n.split(f'{agent} ')[-1]).split(' testing')[0] for n in files]
 
-sel = ['EURUSD', 'USDCHF', 'GBPUSD', 'USDNOK', 'USDSEK'] # european currencies only
-testing_df[sel].mean(1).cumsum().plot()
+    testing_df = [pd.read_csv(file, index_col=0, header=0)[ep] for file in files if 'testing' in file]
+    testing_df = pd.concat(testing_df, axis=1)
+    testing_df.columns = col_names
+
+    # european currencies only
+    sel = ['EURUSD', 'USDCHF', 'GBPUSD', 'USDNOK', 'USDSEK']
+    testing_df = testing_df[sel]
+
+    return testing_df
+
+
+def compute_metrics (rs):
+
+    # Total return
+    total_r = rs.sum()
+
+    # Sharpe ratio
+    sharpe = rs.mean()/rs.std()
+
+    # Maximum Draw-Down
+    current_sum = 0
+    max_sum = 0
+    for n in -rs:
+        current_sum = max(0, current_sum + n)
+        max_sum = max(current_sum, max_sum)
+    max_dd = max_sum
+
+    # Hit ratio
+    hit_ratio = rs.apply(np.sign).replace(-1,0).mean()
+
+    # Results
+    results = pd.Series(data=[total_r, sharpe, max_dd, hit_ratio],
+                        index=['Total Return', 'Sharpe', 'Max DD', 'Hit Ratio'])
+
+    return results
+
+
+rs_gtn = get_testing_df(agent='GTN').mean(1)
+rs_rnn = get_testing_df(agent='RNN').mean(1)
+
+rs_gtn.cumsum().plot()
+rs_rnn.cumsum().plot()
+plt.legend(['GTN', 'RNN'])
+plt.title('Out-of-Sample Performance')
+
+results = pd.concat([compute_metrics(rs_gtn), compute_metrics(rs_rnn)], axis=1)
+results.columns = ['GTN', 'RNN']
+print(results)
